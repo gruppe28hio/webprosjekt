38aeafb4a82192f0c6645a615e98b823281bda3a
alex <alexander.paulsen.89@gmail.com>
2010-10-25 12:59:02 +0200

    oppdatert meny

---
+++ menytest/nedremeny/back.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Backpacking</title>
+</head>
+
+<body>
+<tr>
+			<td><form action='india.php' method='get'/>
+				<input class='nedremeny' type='submit' value='India' name='india'/></form></td>
+		</tr>
+		<tr>
+			<td><form action='pakistan.php' method='get'/>
+				<input class='nedremeny' type='submit' value='Pakistan' name='pakistan'/></form></td>
+		</tr>
+		<tr>
+			<td><form action='peru.php' method='get'/>
+				<input class='nedremeny' type='submit' value='Peru' name='peru'/></form></td>
+		</tr>
+</body>
+</html>
---
+++ menytest/nedremeny/fotball.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Fotballtur</title>
+</head>
+
+<body>
+<tr><td> </td><td></td><td></td>
+			<td><form action='england.php' method='get'/>
+				<input class='nedremeny' type='submit' value='England' name='england'/></form></td>
+		</tr>
+		<tr><td> </td><td></td><td></td>
+			<td><form action='spania.php' method='get'/>
+				<input class='nedremeny' type='submit' value='Spania' name='spania'/></form></td>
+		</tr>
+		<tr><td> </td><td></td><td></td>
+			<td><form action='Frankrike.php' method='get'/>
+				<input class='nedremeny' type='submit' value='Frankrike' name='frankrike'/></form></td>
+		</tr>
+</body>
+</html>
